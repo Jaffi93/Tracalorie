b7ac38049f8dc2427d2d8482e019fbeceeb626ef
mohamed <mohamedjaffi.93@gmail.com>
2023-04-16 12:10:12 +0400

    progress bar and calorie alert functionality added

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ class CalorieTracker {
         this._displayCaloriesConsumed();
         this._displayCaloriesBurned();
         this._displayCaloriesRemaining();
+        this._displayCaloriesProgress();
     }
 
     //Public Methods
@@ -50,8 +51,27 @@ class CalorieTracker {
 
     _displayCaloriesRemaining() {
         const caloriesRemainingEl = document.getElementById('calories-remaining');
+        const progressEl = document.getElementById('calorie-progress');
         const remaining = this._calorieLimit - this._totalCalories;
         caloriesRemainingEl.innerHTML = remaining;
+        if (remaining <= 0) {
+            caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-light');
+            caloriesRemainingEl.parentElement.parentElement.classList.add('bg-danger');
+            progressEl.classList.remove('bg-success');
+            progressEl.classList.add('bg-danger');
+        } else {
+            caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-danger');
+            caloriesRemainingEl.parentElement.parentElement.classList.add('bg-light');
+            progressEl.classList.remove('bg-danger');
+            progressEl.classList.add('bg-success');
+        }
+    }
+
+    _displayCaloriesProgress() {
+        const progressEl = document.getElementById('calorie-progress');
+        const percentage = (this._totalCalories / this._calorieLimit) * 100;
+        const width = Math.min(percentage, 100)
+        progressEl.style.width = `${width}%`
     }
 
     _render() {
@@ -59,6 +79,7 @@ class CalorieTracker {
         this._displayCaloriesConsumed();
         this._displayCaloriesBurned();
         this._displayCaloriesRemaining();
+        this._displayCaloriesProgress();
     }
 }
 
@@ -82,13 +103,9 @@ class Workout {
 
 const tracker = new CalorieTracker()
 
-const breakfast = new Meal('Breakfast', 400)
+const breakfast = new Meal('Breakfast', 1400)
 tracker.addMeal(breakfast)
 
 const run = new Workout('Morning Run', 300)
 tracker.addWorkout(run)
-
-console.log(tracker._meals)
-console.log(tracker._workouts)
-console.log(tracker._totalCalories)
 
